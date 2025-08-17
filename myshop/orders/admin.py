05fdda4d33e5a9546407b934d71d314fd10eedf6
Ulugbek J. <muskisgenius@gmail.com>
2025-08-17 11:42:52 +0500

    pdf  update

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -4,6 +4,16 @@ from django.utils.safestring import mark_safe
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -22,10 +32,10 @@ def export_to_csv(modeladmin, request, queryset):
     content_disposition = f'attachment; filename={opts.verbose_name}.csv'
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = content_disposition
-    write = csv.writer(response)
+    writer = csv.writer(response)
     fields = [field for field in opts.get_fields() if not \
               field.many_to_many and not field.one_to_many]
-    writer.writerow([fields.verbose_name for field in fields])
+    writer.writerow([field.verbose_name for field in fields])
 
     for obj in queryset:
         data_row = []
@@ -42,7 +52,8 @@ export_to_csv.short_description = 'Export to CSV'
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    order_payment, 'created', 'updated']
+                    order_payment, 'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
+
